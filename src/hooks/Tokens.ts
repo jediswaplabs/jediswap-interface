e3b578f3d02a2ec9bd00898b57ec2a11566dc41c
dhruvkelawala <dhruvrk2000@gmail.com>
2022-01-17 16:00:57 +0530

    Add JediTokens to list i.e TOKEN0 and TOKEN1

--- src/hooks/Tokens.ts
+++ src/hooks/Tokens.ts
@@ -1,6 +1,7 @@
-import { Args } from 'starknet'
+import { TOKEN1 } from './../constants/index'
+import { Args, shortString, number as starkNumber } from 'starknet'
 import { parseBytes32String } from '@ethersproject/strings'
-import { Currency, ETHER, Token, currencyEquals } from '@uniswap/sdk'
+import { Currency, TOKEN0, Token, currencyEquals } from '@jediswap/sdk'
 import { useMemo } from 'react'
 import { useSelectedTokenList } from '../state/lists/hooks'
 import { useUserAddedTokens } from '../state/user/hooks'
@@ -9,11 +10,17 @@ import { isAddress } from '../utils'
 import { useActiveStarknetReact } from './index'
 import { useTokenContract } from './useContract'
 import { useStarknetCall, NEVER_RELOAD } from './useStarknet'
-import { BigNumberish } from 'starknet/dist/utils/number'
+// import { BigNumberish } from 'starknet/dist/utils/number'
 
 export function useAllTokens(): { [address: string]: Token } {
   const { chainId } = useActiveStarknetReact()
   const userAddedTokens = useUserAddedTokens()
+
+  const jediTokenMap = useMemo(() => {
+    return {
+      [TOKEN1.address]: TOKEN1
+    }
+  }, [])
   const allTokens = useSelectedTokenList()
 
   return useMemo(() => {
@@ -28,10 +35,10 @@ export function useAllTokens(): { [address: string]: Token } {
           },
           // must make a copy because reduce modifies the map, and we do not
           // want to make a copy in every iteration
-          { ...allTokens[chainId] }
+          { ...allTokens[chainId], ...jediTokenMap }
         )
     )
-  }, [chainId, userAddedTokens, allTokens])
+  }, [chainId, userAddedTokens, allTokens, jediTokenMap])
 }
 
 // Check if currency is included in custom list from user storage
@@ -50,8 +57,13 @@ function parseStringOrBytes32(str: string | undefined, bytes32: string | undefin
     : defaultValue
 }
 
-function parseStringFromArgs(data: any): string | undefined {
+function parseStringFromArgs(data: any, isHexNumber?: boolean): string | undefined {
   if (typeof data === 'string') {
+    if (isHexNumber) {
+      return starkNumber.hexToDecimalString(data)
+    } else if (shortString.isShortString(data)) {
+      return shortString.decodeShortString(data)
+    }
     return data
   }
   return undefined
@@ -62,45 +74,56 @@ function parseStringFromArgs(data: any): string | undefined {
 // otherwise returns the token
 export function useToken(tokenAddress?: string): Token | undefined | null {
   const { chainId } = useActiveStarknetReact()
-  const tokens = useAllTokens()
+  // const tokens = useAllTokens()
 
   const address = isAddress(tokenAddress)
 
-  const tokenContract = useTokenContract(address ? address : undefined, false)
+  const tokenContract = useTokenContract(address ? address : undefined)
+  // console.log('🚀 ~ file: Tokens.ts ~ line 70 ~ useToken ~ tokenContract', tokenContract)
   // const tokenContractBytes32 = useBytes32TokenContract(address ? address : undefined, false)
-  const token: Token | undefined = address ? tokens[address] : undefined
+  // const token: Token | undefined = address ? tokens[address] : undefined
 
-  const { name: tokenName } = useStarknetCall(token ? undefined : tokenContract, 'name', undefined, NEVER_RELOAD)
-  // const tokenNameBytes32 = useSingleCallResult(
-  //   token ? undefined : tokenContractBytes32,
-  //   'name',
-  //   undefined,
-  //   NEVER_RELOAD
-  // )
-  const { symbol } = useStarknetCall(token ? undefined : tokenContract, 'symbol', undefined, NEVER_RELOAD)
+  const { name: tokenName } = useStarknetCall(tokenContract, 'name', undefined, NEVER_RELOAD)
+
+  const { symbol } = useStarknetCall(tokenContract, 'symbol', undefined, NEVER_RELOAD)
+  // console.log('🚀 ~ file: Tokens.ts ~ line 89 ~ useToken ~ symbol', parseStringFromArgs(symbol))
 
   // const symbolBytes32 = useSingleCallResult(token ? undefined : tokenContractBytes32, 'symbol', undefined, NEVER_RELOAD)
-  const { decimals } = useStarknetCall(token ? undefined : tokenContract, 'decimals', undefined, NEVER_RELOAD)
+  const { decimals } = useStarknetCall(tokenContract, 'decimals', undefined, NEVER_RELOAD)
+  // console.log(
+  //   '🚀 ~ file: Tokens.ts ~ line 93 ~ useToken ~ decimals',
+  //   typeof decimals,
+  //   parseStringFromArgs(decimals, true)
+  // )
 
   return useMemo(() => {
-    if (token) return token
-    if (!chainId || !address) return undefined
-    if (!decimals || !symbol) return null
-    if (decimals && typeof decimals === 'string') {
-      return new Token(
+    // if (token) return token
+    if (!chainId || !address) {
+      return undefined
+    }
+    if (!decimals || !symbol) {
+      return null
+    }
+    if (typeof decimals === 'string') {
+      console.log('Building Token')
+
+      const token = new Token(
         chainId,
         address,
         parseInt(decimals),
         parseStringFromArgs(symbol),
         parseStringFromArgs(tokenName)
       )
+      // console.log('🚀 ~ file: Tokens.ts ~ line 112 ~ returnuseMemo ~ token ', token)
+      return token
     }
     return undefined
-  }, [address, chainId, token])
+  }, [address, chainId, decimals, symbol, tokenName])
 }
 
 export function useCurrency(currencyId: string | undefined): Currency | null | undefined {
-  const isETH = currencyId?.toUpperCase() === 'ETH'
-  const token = useToken(isETH ? undefined : currencyId)
-  return isETH ? ETHER : token
+  const isTOKEN0 = currencyId?.toUpperCase() === 'TOKEN0'
+  const token = useToken(isTOKEN0 ? undefined : currencyId)
+  console.log('🚀 ~ file: Tokens.ts ~ line 113 ~ useCurrency ~ token', token)
+  return isTOKEN0 ? TOKEN0 : token
 }
