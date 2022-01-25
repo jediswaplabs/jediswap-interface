85f56a9f973eb71f819bdfc22108aaf8e9053269
dhruvkelawala <dhruvrk2000@gmail.com>
2022-01-25 17:47:22 +0530

    show Tx Status instead of Success on TransactionPopup

--- src/components/Popups/TransactionPopup.tsx
+++ src/components/Popups/TransactionPopup.tsx
@@ -1,3 +1,4 @@
+import { Status } from '@jediswap/starknet'
 import React, { useContext } from 'react'
 import { AlertCircle, CheckCircle } from 'react-feather'
 import styled, { ThemeContext } from 'styled-components'
@@ -14,11 +15,11 @@ const RowNoFlex = styled(AutoRow)`
 
 export default function TransactionPopup({
   hash,
-  success,
+  status,
   summary
 }: {
   hash: string
-  success?: boolean
+  status?: Status
   summary?: string
 }) {
   const { chainId } = useActiveStarknetReact()
@@ -27,14 +28,17 @@ export default function TransactionPopup({
 
   return (
     <RowNoFlex>
-      <div style={{ paddingRight: 16 }}>
-        {success ? <CheckCircle color={theme.green1} size={24} /> : <AlertCircle color={theme.red1} size={24} />}
-      </div>
-      <AutoColumn gap="8px">
-        <TYPE.body fontWeight={500}>{summary ?? 'Hash: ' + hash.slice(0, 8) + '...' + hash.slice(58, 65)}</TYPE.body>
-        {chainId && (
-          <ExternalLink href={getEtherscanLink(chainId, hash, 'transaction')}>View on Etherscan</ExternalLink>
-        )}
+      <AutoColumn>
+        <div style={{ paddingRight: 16 }}>
+          {/* {success ? <CheckCircle color={theme.green1} size={24} /> : <AlertCircle color={theme.red1} size={24} />} */}
+          Transaction Status: {status?.split('_').join(' ')}
+        </div>
+        <AutoColumn gap="8px">
+          <TYPE.body fontWeight={500}>{summary ?? 'Hash: ' + hash.slice(0, 8) + '...' + hash.slice(58, 65)}</TYPE.body>
+          {chainId && (
+            <ExternalLink href={getEtherscanLink(chainId, hash, 'transaction')}>View on Etherscan</ExternalLink>
+          )}
+        </AutoColumn>
       </AutoColumn>
     </RowNoFlex>
   )
