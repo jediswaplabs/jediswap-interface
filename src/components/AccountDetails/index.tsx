0960c62814de4515c941b72fafc7fb8570f618d8
Harshal <harshalmaniya26@gmail.com>
2022-01-26 16:16:12 +0530

    Disconnect Button method changed to deactivate

--- src/components/AccountDetails/index.tsx
+++ src/components/AccountDetails/index.tsx
@@ -362,7 +362,7 @@ export default function AccountDetails({
                 <WalletAction
                   style={{ fontSize: '.875rem', color: '#9B9B9B' }}
                   onClick={() => {
-                    ;(connector as any).close()
+                    ;(connector as any).deactivate()
                   }}
                 >
                   Disconnect
