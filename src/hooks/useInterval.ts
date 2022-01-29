29122df46ecbb50742fd6e4a74f6d90cd7213549
dhruvkelawala <dhruvrk2000@gmail.com>
2022-01-30 01:58:45 +0530

    update useInterval hook as seen in useHooks

--- src/hooks/useInterval.ts
+++ src/hooks/useInterval.ts
@@ -1,25 +1,25 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useLayoutEffect, useRef } from 'react'
 
-export default function useInterval(callback: () => void, delay: null | number, leading = true) {
-  const savedCallback = useRef<() => void>()
+function useInterval(callback: () => void, delay: number | null) {
+  const savedCallback = useRef(callback)
 
-  // Remember the latest callback.
-  useEffect(() => {
+  // Remember the latest callback if it changes.
+  useLayoutEffect(() => {
     savedCallback.current = callback
   }, [callback])
 
   // Set up the interval.
   useEffect(() => {
-    function tick() {
-      const current = savedCallback.current
-      current && current()
+    // Don't schedule if no delay is specified.
+    // Note: 0 is a valid value for delay.
+    if (!delay && delay !== 0) {
+      return
     }
 
-    if (delay !== null) {
-      if (leading) tick()
-      const id = setInterval(tick, delay)
-      return () => clearInterval(id)
-    }
-    return undefined
-  }, [delay, leading])
+    const id = setInterval(() => savedCallback.current(), delay)
+
+    return () => clearInterval(id)
+  }, [delay])
 }
+
+export default useInterval
