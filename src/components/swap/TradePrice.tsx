8edeb903467dd434ba94c931d1152b15f3f9e8da
dhruvkelawala <dhruvrk2000@gmail.com>
2022-01-30 06:35:37 +0530

    update TradePrice component

--- src/components/swap/TradePrice.tsx
+++ src/components/swap/TradePrice.tsx
@@ -1,37 +1,42 @@
 import React from 'react'
-import { Price } from '@jediswap/sdk'
+import { Price, Trade } from '@jediswap/sdk'
 import { useContext } from 'react'
 import { Repeat } from 'react-feather'
 import { Text } from 'rebass'
 import { ThemeContext } from 'styled-components'
 import { StyledBalanceMaxMini } from './styleds'
+import { formatExecutionPrice } from '../../utils/prices'
 
 interface TradePriceProps {
-  price?: Price
+  trade?: Trade
   showInverted: boolean
   setShowInverted: (showInverted: boolean) => void
 }
 
-export default function TradePrice({ price, showInverted, setShowInverted }: TradePriceProps) {
+export default function TradePrice({ trade, showInverted, setShowInverted }: TradePriceProps) {
   const theme = useContext(ThemeContext)
 
-  const formattedPrice = showInverted ? price?.toSignificant(6) : price?.invert()?.toSignificant(6)
+  // const formattedPrice = showInverted ? price?.toSignificant(5) : price?.invert()?.toSignificant(5)
 
-  const show = Boolean(price?.baseCurrency && price?.quoteCurrency)
-  const label = showInverted
-    ? `${price?.quoteCurrency?.symbol} per ${price?.baseCurrency?.symbol}`
-    : `${price?.baseCurrency?.symbol} per ${price?.quoteCurrency?.symbol}`
+  const show = Boolean(trade?.executionPrice?.baseCurrency && trade?.executionPrice?.quoteCurrency)
+  // const label = showInverted
+  //   ? `${price?.quoteCurrency?.symbol} per ${price?.baseCurrency?.symbol}`
+  //   : `${price?.baseCurrency?.symbol} per ${price?.quoteCurrency?.symbol}`
+
+  const formattedPrice = formatExecutionPrice(trade, showInverted, '=')
 
   return (
     <Text
+      fontFamily={'DM Sans'}
       fontWeight={500}
       fontSize={14}
       color={theme.text2}
       style={{ justifyContent: 'center', alignItems: 'center', display: 'flex' }}
+      letterSpacing={'0px'}
     >
       {show ? (
         <>
-          {formattedPrice ?? '-'} {label}
+          {formattedPrice ?? '-'}
           <StyledBalanceMaxMini onClick={() => setShowInverted(!showInverted)}>
             <Repeat size={14} />
           </StyledBalanceMaxMini>
