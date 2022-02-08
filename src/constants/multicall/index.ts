a05c847bda204b26236e1acde38a5381231960f3
dhruvkelawala <dhruvrk2000@gmail.com>
2022-02-08 20:45:54 +0530

    use new multicall for blockTimestamp

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -2,11 +2,11 @@ import { ChainId } from '@jediswap/sdk'
 import MULTICALL_ABI from './abi.json'
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: '0x068d6f9739cf36d07a495069257b25d9cc44342a9bcb216cd6438db552ad3adb',
-  [ChainId.ROPSTEN]: '0x068d6f9739cf36d07a495069257b25d9cc44342a9bcb216cd6438db552ad3adb',
-  [ChainId.KOVAN]: '0x068d6f9739cf36d07a495069257b25d9cc44342a9bcb216cd6438db552ad3adb',
-  [ChainId.RINKEBY]: '0x068d6f9739cf36d07a495069257b25d9cc44342a9bcb216cd6438db552ad3adb',
-  [ChainId.GÖRLI]: '0x068d6f9739cf36d07a495069257b25d9cc44342a9bcb216cd6438db552ad3adb'
+  [ChainId.MAINNET]: '0x536ff7fdf6f4de3caf7bbf85a0ee7f3487d7b05cae1849dd38ffb38abf77381',
+  [ChainId.ROPSTEN]: '0x536ff7fdf6f4de3caf7bbf85a0ee7f3487d7b05cae1849dd38ffb38abf77381',
+  [ChainId.KOVAN]: '0x536ff7fdf6f4de3caf7bbf85a0ee7f3487d7b05cae1849dd38ffb38abf77381',
+  [ChainId.RINKEBY]: '0x536ff7fdf6f4de3caf7bbf85a0ee7f3487d7b05cae1849dd38ffb38abf77381',
+  [ChainId.GÖRLI]: '0x536ff7fdf6f4de3caf7bbf85a0ee7f3487d7b05cae1849dd38ffb38abf77381'
 }
 
 export { MULTICALL_ABI, MULTICALL_NETWORKS }
--- src/hooks/useCurrentBlockTimestamp.ts
+++ src/hooks/useCurrentBlockTimestamp.ts
@@ -2,26 +2,11 @@ import { useEffect, useState, useCallback } from 'react'
 // import { BigNumber } from 'ethers'
 import { useActiveStarknetReact } from './index'
 import { useBlockNumber } from '../state/application/hooks'
+import { useSingleCallResult } from '../state/multicall/hooks'
+import { useMulticallContract } from './useContract'
 
 // gets the current timestamp from the blockchain
 export default function useCurrentBlockTimestamp(): number | undefined {
-  // const multicall = useMulticallContract()
-  // return useSingleCallResult(multicall, 'getCurrentBlockTimestamp')?.result?.[0]
-  const { library } = useActiveStarknetReact()
-  const blockNumber = useBlockNumber()
-
-  const [timestamp, setTimeStamp] = useState<number | undefined>(undefined)
-
-  const callBlockTimestamp = useCallback(() => {
-    if (library && blockNumber) {
-      library.getBlock().then(block => setTimeStamp(block.timestamp))
-    }
-    return undefined
-  }, [library, blockNumber])
-
-  useEffect(() => {
-    callBlockTimestamp()
-  }, [library, blockNumber])
-
-  return timestamp
+  const multicall = useMulticallContract()
+  return useSingleCallResult(multicall, 'get_current_block_timestamp')?.result?.[0]
 }
