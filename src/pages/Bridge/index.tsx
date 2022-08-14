dabc6231a43b2680b33743f8b93aa758d365ed21
Het Modha <hdmodha15@gmail.com>
2022-08-14 22:46:17 +0530

    updated argentx logo.

--- src/pages/Bridge/index.tsx
+++ src/pages/Bridge/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import AppBody from '../AppBody'
 import { JediSwapTabs } from '../../components/NavigationTabs'
 import { Wrapper, BridgeHeaderInfo, WalletDiv } from './styleds'
-import ArgentXLogo from '../../assets/images/argentx.png'
+import ArgentXLogo from '../../assets/images/argentx-white.png'
 import MetamaskLogo from '../../assets/images/metamask.png'
 
 export default function Bridge() {
@@ -12,10 +12,10 @@ export default function Bridge() {
       <Wrapper>
         <BridgeHeaderInfo fontSize={23}>Please connect your Ethereum wallet and Argent wallet</BridgeHeaderInfo>
         <WalletDiv>
-          <img src={ArgentXLogo} width="24px" height="24px" /> 0x520....8848
+          <img src={ArgentXLogo} width="30px" height="30px" /> 0x520....8848
         </WalletDiv>
         <WalletDiv>
-          <img src={MetamaskLogo} width="24px" height="24px" /> MetaMask
+          <img src={MetamaskLogo} width="30px" height="30px" /> MetaMask
         </WalletDiv>
       </Wrapper>
     </AppBody>
