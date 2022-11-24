c38c286accc1e554504e6d537bddfbfc00f8f25d
Lint Action <lint-action@samuelmeuli.com>
2022-11-24 06:57:59 +0000

    Fix code style issues with ESLint

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -5,13 +5,17 @@ import { BraavosConnector } from '@web3-starknet-react/braavos-connector'
 export const NETWORK_CHAIN_ID: number = parseInt(process.env.REACT_APP_CHAIN_ID ?? '5')
 
 const isTestnetHost = () => {
-  const host = (new URL(String(location)))?.host || '';
-  return host === 'app.testnet.jediswap.xyz';
+  const host = new URL(String(location))?.host || ''
+  return host === 'app.testnet.jediswap.xyz'
 }
 
 // Starknet
-export const argentX = new ArgentXConnector({ supportedChainIds: [ChainId.GÖRLI, ...(isTestnetHost() ? [] : [ChainId.MAINNET])] })
+export const argentX = new ArgentXConnector({
+  supportedChainIds: [ChainId.GÖRLI, ...(isTestnetHost() ? [] : [ChainId.MAINNET])]
+})
 
-export const braavosWallet = new BraavosConnector({ supportedChainIds: [ChainId.GÖRLI, ...(isTestnetHost() ? [] : [ChainId.MAINNET])] })
+export const braavosWallet = new BraavosConnector({
+  supportedChainIds: [ChainId.GÖRLI, ...(isTestnetHost() ? [] : [ChainId.MAINNET])]
+})
 
 export type injectedConnector = 'argentx' | 'braavos'
