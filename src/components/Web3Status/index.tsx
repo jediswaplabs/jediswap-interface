a030178b0e4dc2b6b0295fc12f37cf38f9416a87
0xKarasu <0xKarasu@protonmail.com>
2023-02-28 12:12:06 +0100

    :rocket: integrate starknet-id

--- src/components/Web3Status/index.tsx
+++ src/components/Web3Status/index.tsx
@@ -1,7 +1,7 @@
 import { AbstractConnector } from '@web3-starknet-react/abstract-connector'
 import { UnsupportedChainIdError, useStarknetReact } from '@web3-starknet-react/core'
 import { darken, lighten } from 'polished'
-import React, { useMemo } from 'react'
+import React, { useMemo, useEffect, useState } from 'react'
 // import { Activity } from 'react-feather'
 import { useTranslation } from 'react-i18next'
 import styled, { css } from 'styled-components'
@@ -26,6 +26,8 @@ import { RowBetween } from '../Row'
 import WalletModal from '../WalletModal'
 import WrongNetwork from '../../assets/jedi/WrongNetwork.svg'
 import { useActiveStarknetReact } from '../../hooks'
+import { hexToDecimalString } from 'starknet/dist/utils/number'
+
 
 const IconWrapper = styled.div<{ size?: number }>`
   ${({ theme }) => theme.flexColumnNoWrap};
@@ -193,12 +195,14 @@ function StatusIcon({ connector }: { connector: AbstractConnector }) {
   return null
 }
 
-function Web3StatusInner() {
+function Web3StatusInner({
+  starkID
+  }: {
+  starkID?: string
+  }) {
   const { t } = useTranslation()
   const { connectedAddress, connector, error } = useActiveStarknetReact()
-
-  // const { ENSName } = useENSName(connectedAddress ?? undefined)
-
+  
   const allTransactions = useAllTransactions()
 
   const sortedRecentTransactions = useMemo(() => {
@@ -223,7 +227,8 @@ function Web3StatusInner() {
             <Text>{pending?.length} Pending</Text> <Loader stroke="white" />
           </RowBetween>
         ) : (
-          <Text>{shortenAddress(connectedAddress)}</Text>
+          <Text>{starkID ? starkID : shortenAddress(connectedAddress)}</Text>
+          
         )}
       </Web3StatusConnected>
     )
@@ -246,8 +251,20 @@ function Web3StatusInner() {
 export default function Web3Status() {
   const { active, connectedAddress } = useStarknetReact()
   const contextNetwork = useStarknetReact(NetworkContextName)
+  
+  const [domain, setDomain] = useState<any>("");
 
-  // const { ENSName } = useENSName(connectedAddress ?? undefined)
+  useEffect(() => {
+    fetch("https://app.starknet.id/api/indexer/addr_to_domain?addr=" + hexToDecimalString(connectedAddress ?? ""))
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.domain)
+        setDomain(data.domain);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, [connectedAddress]);
 
   const allTransactions = useAllTransactions()
 
@@ -270,8 +287,8 @@ export default function Web3Status() {
 
   return (
     <>
-      <Web3StatusInner />
-      <WalletModal pendingTransactions={pending} confirmedTransactions={confirmed} />
+      <Web3StatusInner starkID={domain}/>
+      <WalletModal pendingTransactions={pending} confirmedTransactions={confirmed} ENSName={domain}/>
     </>
   )
 }
