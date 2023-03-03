a9ab0f51d54725b89c7fd5952e9413df6d1b1a9d
Daniel Constantin <hello@danielconstantin.net>
2023-03-03 15:43:35 +0200

    Feedback from Roman

--- src/pages/Zap/index.tsx
+++ src/pages/Zap/index.tsx
@@ -2,13 +2,16 @@ import React, { useCallback, useContext } from 'react'
 import { SwapPoolTabs } from '../../components/NavigationTabs'
 import { BodyWrapper } from '../AppBody'
 
-import { Backdrop } from '../Swap/styleds'
+import { Backdrop, HeaderRow } from '../Swap/styleds'
 import { useActiveStarknetReact } from '../../hooks'
 import { useWalletModalToggle } from '../../state/application/hooks'
 import { darkTheme, SwapWidget as ZapWidget, isStarknetChain } from 'wido-widget'
 import styled, { ThemeContext } from 'styled-components'
 import { useWeb3React } from '@web3-react/core'
 import { InjectedConnector } from '@web3-react/injected-connector'
+import { AutoColumn } from '../../components/Column'
+import ZapIcon from '../../assets/jedi/zap.svg'
+import { HeaderNote, ZapHeader, ZapHeaderInfo } from './styleds'
 
 export const StyledAppBody = styled(BodyWrapper)`
   padding: 0rem;
@@ -17,7 +20,7 @@ export const injected = new InjectedConnector({})
 
 export default function Zap() {
   const theme = useContext(ThemeContext)
-  const { account, connectedAddress } = useActiveStarknetReact()
+  const { account } = useActiveStarknetReact()
   const { library: ethProvider, activate, deactivate } = useWeb3React()
   const toggleWalletModal = useWalletModalToggle() // toggle wallet when disconnected
 
@@ -42,6 +45,17 @@ export default function Zap() {
 
   return (
     <>
+      <AutoColumn gap="14px" style={{ maxWidth: 470, padding: '2rem' }}>
+        <HeaderRow>
+          <ZapHeader>
+            ZAP <img src={ZapIcon} />
+          </ZapHeader>
+        </HeaderRow>
+        <ZapHeaderInfo fontSize={16}>
+          Zap helps you convert any of your tokens into LP tokens with 1-click
+        </ZapHeaderInfo>
+        <HeaderNote> WARNING: Zaps can cause slippage. Small amounts only.</HeaderNote>
+      </AutoColumn>
       <StyledAppBody>
         <Backdrop top={'0'} left={'503px'} curveRight />
         <Backdrop top={'30px'} left={'493px'} curveRight style={{ height: '60px' }} />
