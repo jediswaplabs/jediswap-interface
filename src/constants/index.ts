b4235461eee2be72716dedf5d51b3d1367924a5c
retroboy <iberezkin90@gmail.com>
2023-03-06 09:16:20 +0100

    updated default_chain_id

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,7 +7,7 @@ import { argentX, braavosWallet } from '../connectors'
 import ARGENTX_ICON from '../assets/images/argentx.png'
 import BRAAVOS_ICON from '../assets/svg/Braavos.svg'
 
-export const DEFAULT_CHAIN_ID = 5
+export const DEFAULT_CHAIN_ID = 1
 
 export const ROUTER_ADDRESS: { [chainId in ChainId]: string } = {
   [ChainId.MAINNET]: validateAndParseAddress('0x41fd22b238fa21cfcf5dd45a8548974d8263b3a531a60388411c5e230f97023'),
