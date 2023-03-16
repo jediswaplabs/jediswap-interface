7021baad32cba84f06a5377af06e3fc9d2028698
Daniel Constantin <hello@danielconstantin.net>
2023-03-16 12:07:36 +0200

    Remove LPs tokens in fromTokens

--- src/pages/Zap/index.tsx
+++ src/pages/Zap/index.tsx
@@ -96,7 +96,24 @@ export default function Zap() {
           ethProvider={ethProvider}
           snAccount={passedAccount}
           testnetsVisible
-          fromTokens={tokenList}
+          // TODO: remove once zap outs are supported
+          fromTokens={[
+            {
+              chainId: 15367,
+              address: '0x5a643907b9a4bc6a55e9069c4fd5fd1f5c79a22470690f75556c4736e34426'
+              // name: 'Goerli USD Coin',
+            },
+            {
+              chainId: 15367,
+              address: '0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7'
+              // name: 'Ether',
+            },
+            {
+              address: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+              chainId: 5
+              // name: 'ETH',
+            }
+          ]}
           toTokens={tokenList}
           theme={{
             ...darkTheme,
