49a55cce8d8614439b98f70d7ed7ddb9aeae4cbb
retroboy <iberezkin90@gmail.com>
2023-05-25 20:00:19 +0200

    added sentry

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import ReactDOM from 'react-dom'
 import ReactGA from 'react-ga4'
 import { Provider } from 'react-redux'
 import { HashRouter } from 'react-router-dom'
+import * as Sentry from "@sentry/react";
 import { NetworkContextName } from './constants'
 import './i18n'
 import App from './pages/App'
@@ -22,6 +23,17 @@ import { StarknetReactProvider, createStarknetReactRoot } from '@web3-starknet-r
 import './components/analytics'
 
 const StarknetProviderNetwork = createStarknetReactRoot(NetworkContextName)
+
+Sentry.init({
+  dsn: "https://6911d3472d9a467b962b9a4b2848abc6@o4505240445911040.ingest.sentry.io/4505240447287296",
+  integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+  release: `${process.env.NPM_PACKAGE_NAME?.replace('/', '_')}@${process.env.NPM_PACKAGE_VERSION}`,
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 function Updaters() {
   return (
@@ -45,6 +57,7 @@ ReactDOM.render(
           <ThemeProvider>
             <ThemedGlobalStyle />
             <HashRouter>
+              <button onClick={() => masdfasdfsdethodDoesNotExist()}>Break the world</button>
               <App />
             </HashRouter>
           </ThemeProvider>
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,58 @@
+import { loadEnv } from "vite";
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  const generateSourcemapsFlag = env.GENERATE_SOURCEMAPS;
+  const isSentryModeEnabled = env.SENTRY_MODE;
+  const sentryAuthToken = env.SENTRY_AUTH_TOKEN;
+  const sentryReleaseName = `${env.npm_package_name.replace('/', '_')}@${env.npm_package_version}`;
+  /*
+    SENTRY_MODE=true запихнуть в отдельный шаг релиза в гитла
+    SENTRY_AUTH_TOKEN нужно добавить в переменные
+    добавить автобамп версий
+   */
+  if (isSentryModeEnabled && !sentryAuthToken) {
+    console.error('process.env.SENTRY_AUTH_TOKEN is not provided')
+  }
+
   return defineConfig({
-    plugins: [react(), svgr()],
+    plugins: [
+      react(),
+      svgr(),
+      (isSentryModeEnabled && sentryAuthToken) && sentryVitePlugin({
+        org: "foo-sb",
+        project: "jediswap-interface",
+
+        // Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
+        // and need `project:releases` and `org:read` scopes
+        authToken: sentryAuthToken,
+        release: {
+          name: sentryReleaseName,
+        },
+        sourcemaps: {
+          // Specify the directory containing build artifacts
+          assets: "./dist/**",
+          // Don't upload the source maps of dependencies
+          ignore: ["./node_modules/**"],
+        },
+
+        // Helps troubleshooting - set to false to make plugin less noisy
+        debug: true,
+      }),
+    ],
     define: {
-      'process.env.NODE_ENV': `"${mode}"`
+      'process.env.NODE_ENV': `"${mode}"`,
+      'process.env.NPM_PACKAGE_NAME': `"${env.npm_package_name}"`,
+      'process.env.NPM_PACKAGE_VERSION': `"${env.npm_package_version}"`,
     },
     build: {
-      chunkSizeWarningLimit: 2048
-      // rollupOptions: {
-      //   input:  './public/index.html',
-      // },
+      chunkSizeWarningLimit: 2048,
+      sourcemap: !!(generateSourcemapsFlag || isSentryModeEnabled),
     },
     test: {
       globals: true,
