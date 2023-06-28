5ecab00d6653e34eed6ce9d47a381ba5f09b3111
retroboy <iberezkin90@gmail.com>
2023-06-28 10:32:46 +0200

    fix uri issue

--- src/hooks/useHttpLocations.ts
+++ src/hooks/useHttpLocations.ts
@@ -3,9 +3,6 @@ import uriToHttp from '../utils/uriToHttp'
 
 export default function useHttpLocations(uri: string | undefined): string[] {
   return useMemo(() => {
-    if (uri?.startsWith('data:')) {
-      return [uri]
-    }
     return uri ? uriToHttp(uri) : []
   }, [uri])
 }
