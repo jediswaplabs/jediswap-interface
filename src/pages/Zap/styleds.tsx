5ad828e61d57cfbd83bc4068fd4c46bf2079b35e
Vinay Singh <122733374+vnaysngh-mudrex@users.noreply.github.com>
2023-08-16 19:13:04 +0530

    staging zap (#208)
    * Add wido-widget in Zap page
    
    * Fix bug in useUserAddedTokens
    
    * Update wido-widget and add toProtocols
    
    * Update widget version
    
    * Feedback from Roman
    
    * Remove zap slippage warning
    
    * Update widget version
    
    * add api.joinwido.com to Content-Security-Policy
    
    * fix Content-Security-Policy connect-src
    
    * Re-create signer when account or network changes
    
    * Update wido-widget
    
    * Set tokenList to Gorli and Starknet Gorli tokens
    
    * Undo work-around
    
    * Add link to zap page from the pool page
    
    * Remove wido package
    
    * Remove LPs tokens in fromTokens
    
    * Update widget version
    
    * Add Bridge page
    
    * Fix Widget title on the Bridge page
    
    * Update widget version
    
    * Fix TokenSelect
    
    * Update Content-Security-Policy
    
    * Update Content-Security-Policy v2
    
    * Prepare for production
    
    * Revert redux version
    
    * Update wido-widget
    
    * Update zap page copy
    
    * Update wido-widget
    
    * Add USDC, DAI and WBTC to Bridging
    
    * Fix switching accounts in Metamask
    
    * Update fromTokens/toTokens
    
    * Update wido-widget
    
    * Update wido-widget
    
    * Address feedback
    
    * Show mainnet by default
    
    * Update wido-widget
    
    * Add Starknet USDT
    
    * Update From Token list
    
    * Use default token list to filter out Jediswap pools
    
    * useAllPairs
    
    * fix no token showing on testnet
    
    * jediswap theming with wido-widget
    
    * wido widget version upgrade to 1.3.0
    
    * Update react-redux
    
    * wido widget version upgrade to 1.4.0
    
    * wido widget version upgrade to 1.5.0
    
    * added zap summary to the interface
    
    * remove bridge from header and avoid eth goerli tx
    
    * bg color fix for buttons
    
    * moved button css to stylesheet
    
    * wido widget version upgrade to 1.5.3
    
    * update jedi lp tokens in zap
    
    * Merge branch 'main' into wido-widget
    
    * fix lp token list type error bug
    
    * update content policy
    
    * removed polling from footer
    
    * polling revert
    
    ---------
    
    Co-authored-by: Daniel Constantin <hello@danielconstantin.net>
    Co-authored-by: Kunal Jain <kunal@joinwido.com>
    Co-authored-by: Kunal Jain <jainkunal3004@gmail.com>
    Co-authored-by: Roman <barabanov1233@gmail.com>

--- src/pages/Zap/styleds.tsx
+++ src/pages/Zap/styleds.tsx
@@ -1,12 +1,11 @@
 import styled from 'styled-components'
-// import { Zap as ZapIcon } from 'react-feather'
 import { DMSansText } from '../../theme'
 
 export const Wrapper = styled.div`
   position: relative;
 `
 
-export const ZapHeader = styled.div`
+export const Header = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -56,7 +55,7 @@ export const HeaderNote = styled.div`
   border-radius: 8px;
 `
 
-export const ZapHeaderInfo = styled(DMSansText.mediumBody)`
+export const HeaderInfo = styled(DMSansText.mediumBody)`
   color: rgba(255, 255, 255, 0.8);
   line-height: 120%;
 `
